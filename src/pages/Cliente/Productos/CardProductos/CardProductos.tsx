3b726c2cfcdf11f631fc48c80a228cb69c7e6223
Daniel Barrientos <danielbarrientos15342@outlook.com>
2025-06-18 02:13:09 -0500

    Refactor addToCart logic to handle pricing and attributes for product variants; ensure fallback values are used when necessary.

--- src/pages/Cliente/Productos/CardProductos/CardProductos.tsx
+++ src/pages/Cliente/Productos/CardProductos/CardProductos.tsx
@@ -123,15 +123,28 @@ const ProductList: React.FC<{
                   <button
                     className="bg-blue-600 hover:bg-blue-700 mt-2 px-4 py-2 rounded w-full font-medium text-white"
                     onClick={() => {
-                      addToCart({
-                        ...producto,
-                        ...(!variante.isFallback && {
-                          precio: variante.precioVenta,
-                          talla: variante.talla,
-                          stockActual: variante.stockActual,
-                          porcentajeDescuento: variante.porcentajeDescuento,
-                        }),
-                      });
+                      addToCart(
+                        {
+                          ...producto,
+                          precio: !variante.isFallback
+                            ? variante.precioVenta
+                            : producto.precio,
+                          precioVenta: !variante.isFallback
+                            ? variante.precioVenta
+                            : producto.precioVenta,
+                          talla: !variante.isFallback
+                            ? variante.talla
+                            : undefined,
+                          stockActual: !variante.isFallback
+                            ? variante.stockActual
+                            : undefined,
+                          porcentajeDescuento: !variante.isFallback
+                            ? variante.porcentajeDescuento
+                            : undefined,
+                          imagenes: producto.imagenes,
+                        },
+                        !variante.isFallback ? variante.talla?.id : undefined,
+                      );
                     }}
                     disabled={!variante.isFallback && variante.stockActual <= 0}
                   >
